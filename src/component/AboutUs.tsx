a3fa00268664cc5f04fee3ad1dd73448f930ff0e
VipulVerma77 <vipulverma4577@gmail.com>
2025-06-04 19:12:28 +0530

    logo

--- src/component/AboutUs.tsx
+++ src/component/AboutUs.tsx
@@ -9,7 +9,7 @@ interface AboutUsProps {
 
 const AboutUs: React.FC<AboutUsProps> = ({ title, description, imageSrc }) => {
   return (
-    <div id="about"  className="flex flex-col-reverse md:flex-row items-center justify-between gap-8 px-8 py-4 md:px-12 md:py-5">
+    <div id="about"  className="flex flex-col md:flex-row items-center justify-between gap-8 md:gap-48 px-8 py-8 md:py-4 md:px-26 md:py-5 mb-0">
       {/* Text Section */}
       <motion.div
         initial={{ x: -100, opacity: 0 }}
@@ -26,7 +26,7 @@ const AboutUs: React.FC<AboutUsProps> = ({ title, description, imageSrc }) => {
         initial={{ x: 100, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{ duration: 0.8 }}
-        className="flex-1 flex justify-center"
+        className="flex-1 flex justify-end"
       >
         <motion.img
           src={imageSrc}
